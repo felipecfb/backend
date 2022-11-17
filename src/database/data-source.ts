3e4d3ba8aa742dfaca8c894ffcb337c76757ae2d
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-11-17 13:43:03 -0300

    ✨ feat: configurando database com typeorm

---
+++ src/database/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+import "reflect-metadata";
+import "dotenv/config";
+
+dotenv.config({ path: "./.env.local" });
+
+const port = process.env.DB_PORT as number | undefined;
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: port,
+  username: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+});
